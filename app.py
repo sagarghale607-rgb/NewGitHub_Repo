5c10a911678f337a6280ffbb8594cb294a398009
unknown <sagarghale607@gmail.com>
2026-05-22 10:02:21 +0545

    Second Commit

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.title('My Name is ...........')
-st.header('Sagar Ghale')
+df = sns.load_dataset("penguins")
+
+
+fig, ax = plt.subplots()
+sns.scatterplot(data=df, x="sex", y="bill_length_mm", hue="bill_length_mm", ax=ax)
+ 
+st.title("Penguin Dataset")
+st.header("This is for the Scatter plot for it")
+st.pyplot(fig)
+st.markdown("**The Table of Penguins**")
+ 
+st.dataframe(df.head())
+st.table(df)
+
+#bill_lenth_mm 
+#sex
